c58b9e3be90dc388aa4643e0f06d5d93b371ccf9
Antishov <i.am.r0ocket@gmail.com>
2017-03-13 20:09:44 +0200

    Allow any values for mockValue

--- index.js
+++ index.js
@@ -32,7 +32,7 @@
    *
    * @param {Object} obj
    * @param {String} key
-   * @param {Function|Value} mockValue
+   * @param {Function|*} mockValue
    * @return {Function} mock
    * @api public
    */
